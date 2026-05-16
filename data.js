463219b999e04849d63198533dd3dd61d7db3af9
Safrayt <WorkOutLands@mail.ru>
2026-05-16 19:14:29 +0300

    Добавление фотографий в репозиторий

--- data.js
+++ data.js
@@ -17,23 +17,6 @@ const places = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ5SyHpg5nPQcJgN4BDty9xWcBD7VSpNEnog&s",
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUuK5ipbL6YxvBaifa1I3u57pAsJAHoA5-6A&s"
     ]
-  },
-  {
-    id: 2,
-    name: "Пляжные турники",
-    lat: 34.019,
-    lng: -118.491,
-    address: "Santa Monica Beach, California",
-    equipment: "Два турника, параллельные брусья",
-    submittedBy: "John Doe",
-    history: [
-      { year: 2015, event: "Установлены" },
-      { year: 2019, event: "Ремонт турников" },
-      { year: 2023, event: "Работают" }
-    ],
-    photos: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnU-zm5LBgl-C14xKfvSTCu1tPLtdKHJZ1bA&s"
-    ]  // если фото нет, оставляем пустой массив
   }
 ];
 
